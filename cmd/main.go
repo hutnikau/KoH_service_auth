270d516c1f329951cad33c37e9864903a9767af3
Aleh Hutnikau <aleh.hutnikau@internetstores.com>
2022-10-16 16:26:02 +0200

    chor: change log level

--- cmd/main.go
+++ cmd/main.go
@@ -30,7 +30,7 @@ func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse
 	reqJson, _ := json.Marshal(req)
 	log.Debug().Msg(string(reqJson))
 
-	log.Debug().RawJSON("jsonfield", reqJson).Msg("Raw json")
+	log.Info().RawJSON("Raw json", reqJson).Msg("Raw json")
 
 	return handlers.UnhandledMethod()
 
